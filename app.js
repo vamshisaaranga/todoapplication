d0ecd6de117463dc457bc402fb581022a6a139fb
vamshisaaranga <vamshisaaranga@gmail.com>
2023-04-20 08:10:53 +0000

    not getting Priority

--- app.js
+++ app.js
@@ -22,7 +22,8 @@ const InitializingDB = async () => {
 };
 InitializingDB();
 
-app.get("/todos/?status=TO%20DO", async (request, response) => {
+//GET when status is TO DO
+app.get("/todos/", async (request, response) => {
   const { status } = request.query;
   const getQuery = `
   SELECT
@@ -30,7 +31,36 @@ app.get("/todos/?status=TO%20DO", async (request, response) => {
   FROM
   todo
   WHERE
-  status LIKE '${status}'`;
-  const details = await db.get(getQuery);
+  status LIKE '%${status}%';`;
+  const details = await db.all(getQuery);
   response.send(details);
+});
+
+//GET WHEN PRIORITY IS HIGH
+
+app.get("/todos/", async (request, response) => {
+  const { priority } = request.query;
+  const getPriority = `
+  SELECT
+  *
+  FROM
+  todo
+  WHERE
+  priority LIKE '%${priority}%';`;
+  const highPriority = await db.get(getPriority);
+  response.send(highPriority);
+});
+//GEt status & priority
+app.get("/todos/", async (request, response) => {
+  const { priority, status } = request.query;
+  const priorityStatusQuery = `
+    SELECT
+    *
+    FROM
+    todo
+    WHERE
+    status LIKE '%${status}' AND
+    priority LIKE '%${priority}';`;
+  const getTodo = await db.all(priorityStatusQuery);
+  response.send(getTodo);
 });
